9e3061c9a71b846dc73409410c32f2d9f6840cb8
Maciej Michalik <halniak24@gmail.com>
2026-02-02 20:41:31 +0100

    algin import path aliases

--- packages/apps/web/vite.config.ts
+++ packages/apps/web/vite.config.ts
@@ -1,4 +1,3 @@
-import path from "path"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
@@ -6,10 +5,12 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), tsconfigPaths(), react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    TanStackRouterVite(),
+    tsconfigPaths({
+      projects: ["./tsconfig.app.json"],
+    }),
+    react(),
+    tailwindcss(),
+  ],
 })
